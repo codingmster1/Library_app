9be1025910cfac7a348be17f43dff086348c8361
Jeff <doylejeff98@gmail.com>
2023-02-22 13:26:36 -0500

    display and book class

--- script.js
+++ script.js
@@ -2,9 +2,35 @@ console.log("hello");
 
 //Book Class
 
-
+class Book
+{
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
 
 //UI Class
+
+class UI {
+    static displayBook() {
+        const StoredBooks = [
+        {
+        title: 'The Black Farm',
+        author: 'Elias Witherow',
+        pages: '257'
+        },
+        {
+        title: 'Berserk',
+        author: 'Kentaro Muira',
+        pages: '250'
+    }
+];
+
+}
+
+}
 
 //Store class: Local Storage
 
