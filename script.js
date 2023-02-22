c8712db7558824d6badf04e2893f514a41b64cc2
Jeff <doylejeff98@gmail.com>
2023-02-22 13:07:54 -0500

    comments added in js for classes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 console.log("hello");
 
-//
+//Book Class
+
+//UI Class
+
+//Store class: Local Storage
+
+//Event Class: Display Book
+
+//Event Class: Add a Book
+
+//Event Class: Remove a Book
+
