306c3081b423c44948bea729cdd5b1096b56941b
Jeff <doylejeff98@gmail.com>
2023-02-24 15:28:34 -0500

    commit

--- script.js
+++ script.js
@@ -82,24 +82,15 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event Class: Add a Book
 
-document.querySelector('#book-form').addEventListener('submit', (e) => 
-{
-
+document.querySelector('#book-form').addEventListener("submit", (e) => {
     //Prevent actual submit
     e.preventDefault();
-
-
-
-    //Get form values
-
+   //Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
-
     const book = new Book(title, author, pages);
-
     console.log(book)
-
 });
 
 
